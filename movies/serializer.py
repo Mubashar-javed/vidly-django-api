25b67dbb732eb265b6865e628cb12f063d3573f2
Mubashar Javed <mubasharbravo302@gmail.com>
2021-01-01 15:42:20 +0500

    add new movie endpoint

--- movies/serializer.py
+++ movies/serializer.py
@@ -5,16 +5,12 @@ from rest_framework import serializers
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = "__all__"
+        fields = ["_id", "name"]
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    genre = GenreSerializer()
+    # genre = GenreSerializer()
 
     class Meta:
         model = Movie
         fields = ["_id", "title", "genre", "numberInStock", "dailyRentalRate"]
-
-    def create(self, validated_data):
-        print("validated data is:", validated_data)
-        return super().create(validated_data)
--- movies/views.py
+++ movies/views.py
@@ -1,26 +1,28 @@
 from rest_framework import generics
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from movies.models import Movie, Genre
 from .serializer import MovieSerializer, GenreSerializer
 
 
-# todo : create endpoint for update movie
+# todo : pending =>> registration view
+# todo : pending ->> this sentence :)
 # todo : Update README.md file
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-    # def create(self, request, *args, **kwargs):
-    #     data = request.data.copy()
-    #     genre = Genre.objects.get(_id=data.pop('genreId'))
-    #     data = dict(**data, genre={"name": genre.name})
-    #     serializer = self.get_serializer(data=data)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_create(serializer)
-    #     headers = self.get_success_headers(serializer.data)
-    #
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        data = dict(**data, genre=data.pop("genreId"))
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class GenreApiView(generics.ListAPIView):
